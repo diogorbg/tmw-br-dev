5f79bb7ca59aa755b1c5d309252a7ca5bde3a8c9
diogorbg <diogorbg@30d2d5f8-7146-11de-9fe7-95ad066c2d19>
2011-03-14 16:55:51 +0000

    Fork do eAthena

---
+++ eathena/src/map/magic-interpreter-parser.h
@@ -0,0 +1,151 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     INT = 258,
+     STRING = 259,
+     ID = 260,
+     DIR = 261,
+     CONST = 262,
+     PROCEDURE = 263,
+     CALL = 264,
+     SILENT = 265,
+     LOCAL = 266,
+     NONMAGIC = 267,
+     SHL = 268,
+     SHR = 269,
+     EQ = 270,
+     NEQ = 271,
+     GTE = 272,
+     LTE = 273,
+     ANDAND = 274,
+     OROR = 275,
+     SCRIPT_DATA = 276,
+     TO = 277,
+     TOWARDS = 278,
+     TELEPORT_ANCHOR = 279,
+     SPELL = 280,
+     LET = 281,
+     IN = 282,
+     END = 283,
+     DARROW = 284,
+     STRING_TY = 285,
+     REQUIRE = 286,
+     CATALYSTS = 287,
+     COMPONENTS = 288,
+     MANA = 289,
+     CASTTIME = 290,
+     SKIP = 291,
+     ABORT = 292,
+     BREAK = 293,
+     EFFECT = 294,
+     ATEND = 295,
+     ATTRIGGER = 296,
+     PC_F = 297,
+     NPC_F = 298,
+     MOB_F = 299,
+     ENTITY_F = 300,
+     TARGET_F = 301,
+     IF = 302,
+     THEN = 303,
+     ELSE = 304,
+     FOREACH = 305,
+     FOR = 306,
+     DO = 307,
+     SLEEP = 308,
+     OR = 309
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1676 of yacc.c  */
+#line 74 "magic-interpreter-parser.y"
+
+    int i;
+    char *s;
+    int op;
+    magic_conf_t *magic_conf;
+    val_t value;
+    expr_t *expr;
+    e_location_t location;
+    e_area_t area;
+    args_rec_t arg_list;
+    struct { int letdefs_nr; letdef_t *letdefs; } letdefs;
+    spell_t *spell;
+    struct { int id, ty; } spellarg_def;
+    letdef_t vardef;
+    spellguard_t *spellguard;
+    component_t *components;
+    struct {int id, count; } component;
+    effect_t *effect;
+    proc_t *proc;
+
+
+
+/* Line 1676 of yacc.c  */
+#line 129 "magic-interpreter-parser.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE magic_frontend_lval;
+
+#if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
+typedef struct YYLTYPE
+{
+  int first_line;
+  int first_column;
+  int last_line;
+  int last_column;
+} YYLTYPE;
+# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+# define YYLTYPE_IS_DECLARED 1
+# define YYLTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYLTYPE magic_frontend_lloc;
+
