02761dbd12171eec715ab125c204ee52daefc7be
diogorbg <diogorbg@30d2d5f8-7146-11de-9fe7-95ad066c2d19>
2011-03-15 20:59:54 +0000

    Logs:  - trabalhando no log do trade.

--- eathena/src/map/atcommand.c
+++ eathena/src/map/atcommand.c
@@ -778,6 +778,62 @@ void log_map(const char *func, struct map_session_data *sd, const char *fmt, ...
 	fflush(logFile);
 }
 
+// FIXME TMW-BR
+void log_trade(const char *func, struct map_session_data *sd, const char *fmt, ...) {
+	char buff[512];
+
+	//- cálculo de tempo...
+	time_t time_v;
+	struct tm t;
+	time (&time_v);
+	localtime_r(&time_v, &t);
+
+	//- recuperando parâmetros...
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(buff, 511, fmt, ap);
+	va_end(ap);
+
+	log_tradeln(func, "'%02d:%02d:%02d',%d,%d,'%s',%d,%d,%s", t.tm_hour, t.tm_min, t.tm_sec,
+			sd->status.account_id, sd->status.char_id,
+			map[sd->bl.m].name, sd->bl.x, sd->bl.y, buff);
+}
+
+// FIXME TMW-BR
+void log_tradeln(const char *func, const char *fmt, ...) {
+	char buff[512];
+
+	//- cálculo de tempo...
+	time_t time_v;
+	struct tm t;
+	time (&time_v);
+	localtime_r(&time_v, &t);
+
+	//- criação do arquivo log_trade...
+    static int timeLogFile = 0;
+	static FILE *logFile = NULL;
+	int time = t.tm_year*12*31 + t.tm_mon*31 + t.tm_mday;
+	if( timeLogFile!=time ) {
+		if(logFile)
+			fclose(logFile);
+		logFile = NULL;
+	}
+	if(!logFile) {
+		timeLogFile = time;
+		sprintf(buff, "log/trade.%04d-%02d-%02d.log", t.tm_year+1900, t.tm_mon+1, t.tm_mday);
+		logFile = fopen(buff, "a");
+	}
+
+	//- recuperando parâmetros...
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(buff, 511, fmt, ap);
+	va_end(ap);
+
+	fprintf(logFile,"%s('%02d:%02d:%02d',%s);\n", func, t.tm_hour, t.tm_min, t.tm_sec, buff);
+	fflush(logFile);
+}
+
 /*==========================================
  *is_atcommand @コマンドに存在するかどうか確認する
  *------------------------------------------
--- eathena/src/map/atcommand.h
+++ eathena/src/map/atcommand.h
@@ -222,6 +222,7 @@ int  msg_config_read (const char *cfgName);
 void log_atcommand (struct map_session_data *sd, const char *fmt, ...);
 void log_storage(const char *func, struct map_session_data *sd, const char *fmt, ...);
 void log_map(const char *func, struct map_session_data *sd, const char *fmt, ...);
+void log_trade(const char *func, struct map_session_data *sd, const char *fmt, ...);
 void gm_log (const char *fmt, ...);
 
 #endif
