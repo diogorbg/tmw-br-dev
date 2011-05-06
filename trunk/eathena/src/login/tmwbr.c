592e5bf7f0784f4908fb5d978a76fc4c89ff2aa6
diogorbg <diogorbg@30d2d5f8-7146-11de-9fe7-95ad066c2d19>
2011-05-06 02:55:31 +0000

    É isso... tudo parece funcionar corretamente. Já pode ir ao jogo.

--- trunk/eathena/src/login/tmwbr.c
+++ trunk/eathena/src/login/tmwbr.c
@@ -1,9 +1,16 @@
 /**
- * funções utilitárias do TMW-BR.
+ * funções utilitárias do login.c - TMW-BR.
+ * obs.: Faz parte do código do login.c e usa a regra de compilação do mesmo.
+ * Ao editar apenas este código edite tbm o login.c para que a compilação seja feita corretamente. :P
  */
 
 #include <glib.h>
 
+#define DEBUG() //printf("# login linha:%d\n", __LINE__);
+#define DEBUGD(d) //printf("# login linha:%d var:%d\n", __LINE__, (d));
+#define DEBUGS(s) //printf("# login linha:%d var:%s\n", __LINE__, (s));
+
+/*
 typedef struct InfoLogin {
 	unsigned int time;
 	unsigned int ignorados;
@@ -19,6 +26,7 @@ gpointer newInfoLogin(unsigned int time) {
     new->acertos = 0;
 	return (gpointer)new;
 }
+*/
 
 void freeGPoniter(gpointer data) {
 	free(data);
@@ -29,3 +37,52 @@ gpointer newInt(int i) {
     *new = i;
 	return (gpointer)new;
 }
+
+/**
+ * Critica a senha caso ela seja muito simples.
+ * Criticizes the password if it is very simple.
+ *  0: ok.
+ *  1: A senha deve ter no mínimo 8 caracteres.
+ *  2: A senha deve conter números.
+ *  4: A senha deve conter letras minúsculas.
+ *  8: A senha deve conter letras maiúsculas.
+ * 16: A senha deve conter símbolos. (não será obrigatório)
+ * 15: Soma de todas as críticas.
+ */
+int criticalPasswd(unsigned char *str) {
+	int i;
+	int num = 0;
+
+	for(i=0; str[i]; i++) {
+		if( str[i]<32 )
+			continue;
+		else if( str[i]>='0' && str[i]<='9' )
+			num = num | 2;
+		else if( str[i]>='a' && str[i]<='z' )
+			num = num | 4;
+		else if( str[i]>='A' && str[i]<='Z' )
+			num = num | 8;
+		//else
+		//	num = num | 16;
+	}
+	if(i>=8)
+		num = num | 1;
+
+	//- Retorna condições não saciadas pela crítica.
+	return 15 & ~num;
+}
+
+/*
+void listCriticalPasswd(int criticalPasswd){
+	if( criticalPasswd&1 )
+		printf("* A senha deve ter no mínimo 8 caracteres.\n");
+	if( criticalPasswd&2 )
+		printf("* A senha deve conter números.\n");
+	if( criticalPasswd&4 )
+		printf("* A senha deve conter letras minúsculas.\n");
+	if( criticalPasswd&8 )
+		printf("* A senha deve conter letras maiúsculas.\n");
+	if( criticalPasswd&16 )
+		printf("* A senha deve conter símbolos.\n");
+}
+*/
