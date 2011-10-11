452b8d352ec5da583a95773c114025cba76b0cc1
diogorbg <diogorbg@30d2d5f8-7146-11de-9fe7-95ad066c2d19>
2011-10-11 01:50:40 +0000

    Dados lado a lado.

--- parseLogs/src/CountUsers.java
+++ parseLogs/src/CountUsers.java
@@ -1,5 +1,7 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import parse.ProcessMap;
 import bean.Header;
@@ -8,74 +10,131 @@ import bean.Status;
 
 public class CountUsers extends parse.Process implements ProcessMap {
 
-	private int count = 0;
+	private static int TEMPO = 15;
+	private int count=0, max=0;
+	private int nLogin=0, nLogout=0;
 	private Date date = null;
 
-	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+	private String title = "";
+	private String[] tab = new String[24*60/TEMPO];
+	//private HashMap<String,String> tab = new HashMap<String,String>();
+
+	//private SimpleDateFormat fmt1 = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+	private SimpleDateFormat fmt2 = new SimpleDateFormat("HH:mm");
 
 	public CountUsers() {
 	}
 
+	public void print() {
+		System.out.println(title);
+		for (int i=0; i<tab.length; i++) {
+			System.out.println(tab[i]);
+		}
+		//for (Entry<String, String> obj : tab.entrySet()) {
+		//	System.out.println(obj.getKey()+","+obj.getValue());
+		//}
+	}
+
+	@SuppressWarnings("deprecation")
 	public boolean processLogin(Header header, Map map, Status status) {
 
 		//if(status.getLvlGm()>0 || status.getLvl() < 70)
 		//	return true;
 		//if (header.getIdAcc()!=2005693) return true;
 
+		add();
+
 		Date d1 = (Date) header.getDate().clone();
 		d1.setSeconds(0);
-		d1.setMinutes( (d1.getMinutes()/10) * 10 );
+		d1.setMinutes( (d1.getMinutes()/TEMPO) * TEMPO );
 		if (date==null) {
 			date = (Date) d1.clone();
-		} else if (d1.compareTo(date)>0) {
+			date.setMinutes(0);
+			date.setHours(0);
+		}
+		if (d1.compareTo(date)>0) {
 			Date d2 = (Date) date.clone();
 			while (d1.compareTo(d2)>0) {
-				System.out.println(sdf.format(d2)+ ",qtd " +getCount()+ ",login");
-				d2.setTime( d2.getTime()+600000 );
+				addTab(d2);
+				//System.out.println(fmt1.format(d2)+ ",qtd " +max()+ ",login");
+				d2.setTime( d2.getTime()+(TEMPO*60000) );
 			}
 			date = (Date) d1.clone();
+			zerar();
 		}
-
-		add();
 
 		//System.out.println(sdf.format(header.getDate())+ ", qtd " +getCount()+ ", login, lvl " +status.getLvl()+ ", lvlGM " +status.getLvlGm());
 		//System.out.println("* Login do jogador "+header.getIdChr()+" de ip '"+header.getIp()+"' em '"+header.getTimer()+"'");
 		return true;
 	}
 
+	@SuppressWarnings("deprecation")
 	public boolean processLogout(Header header, Map map, Status status) {
 		//if(status.getLvlGm()>0 || status.getLvl() < 70)
 		//	return true;
 		//if (header.getIdAcc()!=2005693) return true;
 
+		del();
+
 		Date d1 = (Date) header.getDate().clone();
 		d1.setSeconds(0);
-		d1.setMinutes( (d1.getMinutes()/10) * 10 );
+		d1.setMinutes( (d1.getMinutes()/TEMPO) * TEMPO );
 		if (date==null) {
 			date = (Date) d1.clone();
-		} else if (d1.compareTo(date)>0) {
+			date.setMinutes(0);
+			date.setHours(0);
+		}
+		if (d1.compareTo(date)>0) {
 			Date d2 = (Date) date.clone();
 			while (d1.compareTo(d2)>0) {
-				System.out.println(sdf.format(d2)+ ",qtd " +getCount()+ ",logout");
-				d2.setTime( d2.getTime()+600000 );
+				addTab(d2);
+				//System.out.println(fmt1.format(d2)+ ",qtd " +max()+ ",logout");
+				d2.setTime( d2.getTime()+(TEMPO*60000) );
 			}
 			date = (Date) d1.clone();
+			zerar();
 		}
-
-		del();
 
 		//System.out.println(sdf.format(header.getDate())+ ", qtd " +getCount()+ ", logout, lvl " +status.getLvl()+ ", lvlGM " +status.getLvlGm());
 		//System.out.println("* Logout do jogador "+header.getIdChr()+" de ip '"+header.getIp()+"' em '"+header.getTimer()+"'");
 		return true;
 	}
 
+	@SuppressWarnings("deprecation")
+	private void addTab(Date d2) {
+		String key = fmt2.format(d2);
+		if(key.equals("00:00")) {
+			if(title.length()==0)
+				title = "hora";
+			title += String.format(",%02d", d2.getDate());
+		}
+		int i = d2.getHours()*60/TEMPO +  d2.getMinutes()/TEMPO;
+		if(tab[i]==null) {
+			tab[i] = ""+key;
+		}
+		tab[i] += ","+max();
+		//String vlr = tab.get(key);
+		//if(vlr==null) {
+		//	vlr = ""+max();
+		//	tab.put(key, vlr);
+		//} else {
+		//	vlr += ","+max();
+		//}
+		//System.out.println(key+","+max());
+	}
+
 	public void add() {
-		count+= 1;
+		nLogin++;
+		count += 1;
+		if(count>max)
+			max = count;
 	}
 
 	public void del() {
-		if(count>0)
-			count-= 1;
+		if(nLogout==nLogin)
+			return;
+		nLogout++;
+		count -= 1;
 	}
 
 	public int getCount() {
@@ -84,6 +143,14 @@ public class CountUsers extends parse.Process implements ProcessMap {
 
 	public void setCount(int count) {
 		this.count = count;
+	}
+
+	public int max() {
+		return max>count ? max : count;
+	}
+
+	public int zerar() {
+		return max = count;
 	}
 
 }
