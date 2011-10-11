d05960584aad85d0356361638cf7e621127e2fd9
diogorbg <diogorbg@30d2d5f8-7146-11de-9fe7-95ad066c2d19>
2011-10-11 01:50:40 +0000

    Dados lado a lado.

--- trunk/parseLogs/src/ParserLogs.java
+++ trunk/parseLogs/src/ParserLogs.java
@@ -2,15 +2,15 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import parse.ParserLog;
-import parse.Process;
 
 public class ParserLogs {
 
 	public static void main(String[] args) {
 		ParserLog parser = new ParserLog();
-		Process users = new CountUsers();
+		CountUsers users = new CountUsers();
 		try {
-			parser.loadMap("/home/diogorbg/workspace2/parseLogs/map.log", users);
+			parser.loadMap("/home/diogorbg/workspace/parseLogs/map.log", users);
+			users.print();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
