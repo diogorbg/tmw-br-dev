8ffe2e9f33a6dc757a6c5509da430aee1191caa1
diogorbg <diogorbg@30d2d5f8-7146-11de-9fe7-95ad066c2d19>
2011-12-23 15:57:31 +0000

    MapDecoder  - Nome dos Chars

--- trunk/parseLogs/src/MapDecoder.java
+++ trunk/parseLogs/src/MapDecoder.java
@@ -1,23 +1,36 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
 
+import bean.Char;
 import bean.Header;
-import bean.Map;
 import bean.Status;
+import parse.ParserChars;
 import parse.ParserLog;
 import parse.ProcessMap;
-
 
 public class MapDecoder extends parse.Process implements ProcessMap {
 
 	private SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	private static Map<Integer,Char> mapChars = null;
+	private static Set<Integer> setChars = new LinkedHashSet<Integer>();
 
 	public static void main(String[] args) {
 		ParserLog parser = new ParserLog();
 		parse.Process decoder = new MapDecoder();
 		try {
-			parser.loadMap("/home/diogorbg/workspace2/parseLogs/map.log", decoder);
+			mapChars = ParserChars.loadChars("../db/chars.db");
+			parser.loadMap("../map.log", decoder);
+			System.out.println("\n.:: Contas Envolvidas ::.");
+			for(Integer obj : setChars) {
+				Char chr = mapChars.get( new Integer(obj) );
+				if (chr==null)
+					chr = new Char(obj, 0, "<nome?>", 0, 0);
+				System.out.println(chr.getName()+"["+chr.getLvl()+"]("+chr.getIdAcc()+")");
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -27,14 +40,18 @@ public class MapDecoder extends parse.Process implements ProcessMap {
 		}
 	}
 
-	public boolean processLogin(Header header, Map map, Status status) {
-		String str = String.format("%s,%s,%d,%d, ip:%s, map:%s,%d,%d, lvl:%d,%d, gm:%d", header.getCmd(), fmt.format(header.getDate()), header.getIdAcc(),
-				header.getIdChr(), header.getIp(), map.getMap(), map.getX(), map.getY(), status.getLvl(), status.getXp(), status.getLvlGm());
+	public boolean processLogin(Header header, bean.Map map, Status status) {
+		setChars.add( header.getIdChr() );
+		Char chr = mapChars.get( new Integer(header.getIdChr()) );
+		if (chr==null)
+			chr = new Char(header.getIdChr(), 0, "<nome?>", 0, 0);
+		String str = String.format("%s,%s,%d,%s[%d](%d), ip:%s, map:%s,%d,%d, lvl:%d,%d, gm:%d", header.getCmd(), fmt.format(header.getDate()), header.getIdAcc(),
+				chr.getName(),chr.getLvl(),header.getIdChr(), header.getIp(), map.getMap(), map.getX(), map.getY(), status.getLvl(), status.getXp(), status.getLvlGm());
 		System.out.println(str);
 		return true;
 	}
 
-	public boolean processLogout(Header header, Map map, Status status) {
+	public boolean processLogout(Header header, bean.Map map, Status status) {
 		String str = String.format("%s,%s,%d,%d, ip:%s, map:%s,%d,%d, lvl:%d,%d, gm:%d", header.getCmd(), fmt.format(header.getDate()), header.getIdAcc(),
 				header.getIdChr(), header.getIp(), map.getMap(), map.getX(), map.getY(), status.getLvl(), status.getXp(), status.getLvlGm());
 		System.out.println(str);
